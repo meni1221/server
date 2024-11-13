08fa180c75abcddbf79fb16e77bada8cc5b90107
Meni <menil@MENI.com>
2024-11-13 17:26:20 +0200

    Created Missilse Model

---
+++ src/Modules/MissileModel.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IMissile extends Document {
+  _id: string;
+  name: string;
+  description: string;
+  speed: number;
+  intercepts: [{type: string}];
+  price: number;
+}
+
+const MissileSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  speed: {
+    type: Number,
+    required: true,
+  },
+  intercepts: {
+    type: [{type: String}],
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+
+export default mongoose.model<IMissile>("Missile", MissileSchema); 
